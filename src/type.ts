36a6b46488ad512ed50ac0e200c28200d8a4369b
lfp1024 <lfp1024@126.com>
2018-10-10 19:16:13 +0800

    type

---
+++ src/type.ts
@@ -0,0 +1,19 @@
+type of = {
+    name: "lfp",
+    age: number
+}
+
+type off = {
+    name: string,
+    age: number
+}
+
+const ls: of = {
+    name: "lfp",
+    age: 23
+}
+
+const lss: off = {
+    name: "lfp",
+    age: 26
+}
